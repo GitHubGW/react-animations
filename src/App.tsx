6a188a071e73da1fce76a14054fe2f1ae1654eb2
GW <kowonp@gmail.com>
2022-03-13 03:46:05 +0900

    feat(Animation.tsx): use animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import styled from "styled-components";
+import Animation from "./components/Animation";
+import GlobalStyle from "./GlobalStyle";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <GlobalStyle />
+      <Animation />
+    </Wrapper>
+  );
 };
 
 export default App;
